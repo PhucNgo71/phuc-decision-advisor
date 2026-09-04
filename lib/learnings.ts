de0cc39d9356fc39794b1f9772e119c95f4ea452
PhucNgo71 <phuc.ngovh@gmail.com>
2026-09-04 23:21:18 +0700

    Add learning import helpers

---
+++ lib/learnings.ts
@@ -0,0 +1,12 @@
+import imports from '../data/learning-imports.json';
+import type { LearningImport } from './types';
+
+export const learningImports = imports as LearningImport[];
+
+export const reviewedLearningCount = learningImports
+  .filter((learningImport) => learningImport.reviewStatus === 'reviewed')
+  .flatMap((learningImport) => learningImport.records)
+  .filter((record) => record.adviceImpact !== 'none').length;
+
+export const latestLearningImport = learningImports.at(-1);
+
